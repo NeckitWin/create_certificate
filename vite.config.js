49a4a2defb6c5a8d7871009612d7484a4602f834
NeckitWin <neckit.v@gmail.com>
2024-10-07 23:37:42 +0200

    change path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/certificate_create/',
+    base: '/create_certificate/',
     plugins: [react()],
     build: {
         outDir: 'build'
